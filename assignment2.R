7ad28637b1fc4d78931c1ecf9a8ba29ebdb92c84
SammualXYuan <yuanx621@gmail.com>
2018-05-24 22:16:09 +1000

    my revised answer

--- assignment2.R
+++ assignment2.R
@@ -1,39 +1,30 @@
 ## Cache inverse of a matrix
-## How to use it:
-## 1) define da, inverse as NULL
-## 2) Define a matrix  
-## 3) call the nested function
-## the code in the console should be like:
 
-# source("cachematrix.R")
-# da <- NULL
-# inverse <- NULL
-# d <- matrix(rnorm(9,1,0.4),3,3)
-# cacheSolve(makeCacheMatrix(d))
-# cacheSolve(makeCacheMatrix(d))
-# d <- matrix(rnorm(9,2,0.1),3,3)
-# cacheSolve(makeCacheMatrix(d))
-# cacheSolve(makeCacheMatrix(d))
-
-## set a list of functions that will be called in cacheSolve
+## set a list of functions that will be called in cacheSolve (get rid of set function)
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
   get <- function() x
-  getInverse <- function() inverse
-  list(get=get,getInverse=getInverse)
+  setInverse <- function(inverse) m <<- inverse
+  getInverse <- function() m
+  list(get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
-
-## Call get() and getInverse()
+## Call the functions and calculate the inverse
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  if(identical(da, x$get())) {
-    message("getting cached data")
-    return(inverse)
-    break
-  }
-  da <<- x$get()
-  inverse <<- solve(da, ...)
-  inverse
+    m <- x$getInverse()
+    if(!is.null(m)) {
+      message("getting cached data")
+      return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setInverse(m)
+    m 
 }
+  
+
